3bec0afb9b433ef20fbd06d5e4b949f8d02c371f
ab-dereje <abudereje09@gmail.com>
2024-07-13 23:38:07 +0300

    connecting with mongodb is done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb+srv://abdi:abdi@cluster0.8y7a2c4.mongodb.net/mydatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
+
+// Example route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
